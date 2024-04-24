e4fdd81cb1166f262a6fcc2f37d2ca822509ce84
张尧 <zhangyao@ft.tech>
2024-04-24 13:47:35 +0800

    chore: iterface

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from hanlp_restful import HanLPClient
 from openai import OpenAI
 import tqdm
 from database import NDB
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from response import CommonResponse
 from contextlib import asynccontextmanager
+from pydantic import BaseModel
+
 
 client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
 HanLP = HanLPClient('https://www.hanlp.com/api', auth='NTAyN0BiYnMuaGFubHAuY29tOkw5clFLbGhTRTIyNUppUDE=',
@@ -102,9 +104,12 @@ async def clear_all():
     NDB.clear_all()
     return CommonResponse.success("清空成功")
 
+class AddArticleBody(BaseModel):
+    text: str
 
 @app.post("/articles", summary="插入文章, 并提取三元组，存入图数据库")
-async def add_articles(text: str):
+async def add_articles(body: AddArticleBody = Body()):
+    text = body.text
     sents = summarize(text)
     result = get_triple_groups(sents)
     for i in result:
